a0367b60785006b7571b0eed43042c56417e7cd6
eysaif <u4saif@gmail.com>
2023-05-17 12:27:09 +0530

    connected with mongo db and added loadtest

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const colors = require('./utilities/consoleColors');
+const dbConnect = require('./config/db');
+const users = require("./models/usersModel");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.get('/',(req,res)=>{
-    // res.send("Hello Saif");
-    data = { name : 'Saif',age : 23};
+//Load local Env
+dotenv.config({ path: './config/config.env' })
+dbConnect(); 
+
+function delayResponse(duration){
+    const start = Date.now();
+    while(Date.now() - start < duration){}
+}
+app.get('/', async (req,res)=>{
+    data = { username : 'Saif', password : 23};
+    //const userData = await users.find();
+    delayResponse(5000)
+    //console.log(userData);
     res.status(200).json(data);
 })
 
 
 app.listen(PORT , ()=>{
-    console.log("App is Running on PORT:".info,PORT);
+    console.log("🚀🚀App is Running on 🚨PORT:".info,PORT);
 })
